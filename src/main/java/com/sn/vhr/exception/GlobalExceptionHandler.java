45a4a63865f6201af1094c513ddb96b3fcc7ad55
SheHuan <she320955huan>
2021-03-04 23:27:20 +0800

    异常处理

--- src/main/java/com/sn/vhr/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sn/vhr/exception/GlobalExceptionHandler.java
@@ -15,8 +15,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(SQLException.class)
     public RespBean sqlException(SQLException e) {
         if (e instanceof SQLIntegrityConstraintViolationException) {
-            return RespBean.error("该数据有关联数据，操作失败!");
+            if (e.getMessage().contains("Duplicate entry")) {
+                return RespBean.error("数据已存在，请勿重复添加！");
+            }
+            return RespBean.error("该数据有关联数据，操作失败！");
         }
-        return RespBean.error("数据库异常，操作失败!");
+        return RespBean.error("数据库异常，操作失败！");
     }
 }
